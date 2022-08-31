2f8c5619a8a9ad588db66189b5d25e5b198f2e3c
Suliman Fayyaz <hydrajin@gmail.com>
2022-08-30 23:38:58 -0400

    Add catch unclosed parentheses ...

---
+++ 5_catch_unclosed_parentheses_brackets_braces_and_quotes.js
@@ -0,0 +1,3 @@
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`); // Sum of array values is: 6
