038af47f0504c1e3366cca3a37d71e93290e34cd
Suliman Fayyaz <hydrajin@gmail.com>
2022-09-01 15:30:55 -0400

    Add catch missing open and closing parenthesis after a function call

---
+++ 8_catch_missing_open_and_closing_parenthesis_after_a_function_call.js
@@ -0,0 +1,11 @@
+function getNine() {
+  let x = 6;
+  let y = 3;
+  return x + y;
+}
+
+let result = getNine();
+console.log(result); // 9
+
+// let result = getNine (result would be the function getNine)
+// let result = getNine() (result would be the return value from calling function getNine)
