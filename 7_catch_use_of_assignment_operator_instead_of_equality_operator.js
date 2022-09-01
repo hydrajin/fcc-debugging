20fc0bfc7236516a8c3e160aebc1aca62d4d86fa
Suliman Fayyaz <hydrajin@gmail.com>
2022-09-01 13:58:22 -0400

    Add catch use of assignment operator instead of equality operator

---
+++ 7_catch_use_of_assignment_operator_instead_of_equality_operator.js
@@ -0,0 +1,12 @@
+let x = 7;
+let y = 9;
+let result = "to come";
+
+// x = y is using an assignment operator which would result in a truthy value
+if (x === y) {
+  result = "Equal!";
+} else {
+  result = "Not equal!";
+}
+
+console.log(result);
